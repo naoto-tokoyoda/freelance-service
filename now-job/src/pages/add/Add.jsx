8cbe84fbe1b0b8ceaac5a88deada2bebdce2a84b
Naoto Tokoyoda <tkyd89@gmail.com>
2023-10-13 16:35:57 +0900

    organized codes

--- now-job/src/pages/add/Add.jsx
+++ now-job/src/pages/add/Add.jsx
@@ -5,14 +5,16 @@ const Add = () => {
   return (
     <div className="add">
       <div className="container">
+        
         <h1>Add New Gig</h1>
+        
         <div className="sections">
+          
+          {/* Info Section */}
           <div className="info">
             <label htmlFor="">Title</label>
-            <input
-              type="text"
-              placeholder="e.g. I will do something I'm really good at"
-            />
+            <input type="text" placeholder="e.g. I will do something I'm really good at" />
+
             <label htmlFor="">Category</label>
             <select name="cats" id="cats">
               <option value="design">Design</option>
@@ -20,31 +22,55 @@ const Add = () => {
               <option value="animation">Animation</option>
               <option value="music">Music</option>
             </select>
+
             <label htmlFor="">Cover Image</label>
             <input type="file" />
+
             <label htmlFor="">Upload Images</label>
             <input type="file" multiple />
+
             <label htmlFor="">Description</label>
-            <textarea name="" id="" placeholder="Brief descriptions to introduce your service to customers" cols="0" rows="16"></textarea>
+            <textarea 
+              name="" 
+              id="" 
+              placeholder="Brief descriptions to introduce your service to customers" 
+              cols="0" 
+              rows="16">
+            </textarea>
+
             <button>Create</button>
           </div>
+          
+          {/* Details Section */}
           <div className="details">
             <label htmlFor="">Service Title</label>
             <input type="text" placeholder="e.g. One-page web design" />
+
             <label htmlFor="">Short Description</label>
-            <textarea name="" id="" placeholder="Short description of your service" cols="30" rows="10"></textarea>
+            <textarea 
+              name="" 
+              id="" 
+              placeholder="Short description of your service" 
+              cols="30" 
+              rows="10">
+            </textarea>
+
             <label htmlFor="">Delivery Time (e.g. 3 days)</label>
             <input type="number" />
+
             <label htmlFor="">Revision Number</label>
             <input type="number" />
+
             <label htmlFor="">Add Features</label>
             <input type="text" placeholder="e.g. page design" />
             <input type="text" placeholder="e.g. file uploading" />
             <input type="text" placeholder="e.g. setting up a domain" />
             <input type="text" placeholder="e.g. hosting" />
+
             <label htmlFor="">Price</label>
             <input type="number" />
           </div>
+          
         </div>
       </div>
     </div>
