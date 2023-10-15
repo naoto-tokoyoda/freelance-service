63c640d55beb958684827db7bc73c9ece76aa671
Naoto Tokoyoda <tkyd89@gmail.com>
2023-10-15 14:48:26 +0900

    server setup

--- api/server.js
+++ api/server.js
@@ -1,1 +1,32 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Initialize dotenv
+dotenv.config();
+
+// Configure Mongoose
+mongoose.set('strictQuery', true);
+
+const app = express();
+
+// MongoDB connection function
+const connectToMongoDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongoDB!");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+};
+
+// Start the Express server
+const startServer = () => {
+    app.listen(8800, () => {
+        console.log("Backend server is running on port 8800");
+    });
+    connectToMongoDB();
+};
+
+// Start the server
+startServer();
